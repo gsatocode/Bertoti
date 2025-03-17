5bfcc627862d6bbbd8945732e2b60c2808b3d6c7
thenewjapzzz <guilherme.yokota20@gmail.com>
2025-03-17 08:17:36 -0300

    observer

---
+++ eng3/src/observer/Main.java
@@ -0,0 +1,18 @@
+package observer;
+
+public class Main {
+    public static void main(String[] args) {
+        ConcreteSubject subject = new ConcreteSubject();
+
+        Observer observer1 = new ConcreteObserver("Observer 1");
+        Observer observer2 = new ConcreteObserver("Observer 2");
+
+        subject.addObserver(observer1);
+        subject.addObserver(observer2);
+
+        subject.notifyObservers("New event occurred!");
+
+        subject.removeObserver(observer1);
+        subject.notifyObservers("Another event occurred!");
+    }
+}
